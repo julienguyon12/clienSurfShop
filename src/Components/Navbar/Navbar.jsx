667394ff6922ad2e821e0ae0ad583dbc6a84ddbc
julienguyon12 <julien.guyon@2018.icam.fr>
2023-09-19 13:33:15 +0200

    change id categorie in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import ReorderIcon from '@mui/icons-material/Reorder';
-import SearchIcon from '@mui/icons-material/Search';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import { Link, useLocation } from 'react-router-dom';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import './Navbar.scss';
-import Cart from '../Cart/Cart';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import ReorderIcon from "@mui/icons-material/Reorder";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link, useLocation } from "react-router-dom";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import "./Navbar.scss";
+import Cart from "../Cart/Cart";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -18,9 +18,9 @@ const Navbar = () => {
     setExpendNavbar(false);
   }, [location]);
   return (
-    <div className='navbar' id={expendNavbar ? 'on' : 'off'}>
-      <div className='wrapper'>
-        <div className='toggleButton'>
+    <div className="navbar" id={expendNavbar ? "on" : "off"}>
+      <div className="wrapper">
+        <div className="toggleButton">
           <button
             onClick={() => {
               setExpendNavbar((prev) => !prev);
@@ -29,23 +29,23 @@ const Navbar = () => {
             <ReorderIcon />
           </button>
         </div>
-        <div className='links'>
-          <Link to='/'>Home</Link>
-          <Link to='/products/1'>Shortboards</Link>
-          <Link to='/products/2'>Longboards</Link>
-          <Link to='/products/3'>Funboards</Link>
-          <Link to='/products/4'>Accessories</Link>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/products/1">Shortboards</Link>
+          <Link to="/products/3">Longboards</Link>
+          <Link to="/products/2">Funboards</Link>
+          <Link to="/products/4">Accessories</Link>
         </div>
-        <div className='center'>
-          <Link to='/' className='link'>
+        <div className="center">
+          <Link to="/" className="link">
             MonP'titSurfShop
           </Link>
         </div>
-        <div className='icons'>
-          <SearchIcon className='icon' />
-          <PersonOutlineIcon className='icon' />
-          <FavoriteBorder className='icon' />
-          <div className='carticon' onClick={(e) => setOpen(!open)}>
+        <div className="icons">
+          <SearchIcon className="icon" />
+          <PersonOutlineIcon className="icon" />
+          <FavoriteBorder className="icon" />
+          <div className="carticon" onClick={(e) => setOpen(!open)}>
             <ShoppingCartOutlinedIcon />
             <span>{products.length}</span>
           </div>
